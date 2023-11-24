721bc730a8b3bf8b04022162d3b8895bebfd388a
Presencial <Presencial@DESKTOP-LPHGJF8>
2023-11-24 09:22:35 +0100

    2 comit

--- EjemploGit/src/main/java/Principal/Principal.java
+++ EjemploGit/src/main/java/Principal/Principal.java
@@ -4,8 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("hola GIT");
-
+		System.out.println("ya se usar git");
+		System.out.println("hola");
 		
+
 		
 	}
 
